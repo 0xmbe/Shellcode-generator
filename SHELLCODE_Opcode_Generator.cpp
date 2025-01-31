79b1d2bbfbf3502b8aa095f5f257921b1132caf6
0xmbe <matija.bensa@gmail.com>
2025-02-01 00:33:02 +0100

    Added input, output types, multi argument input

--- SHELLCODE_Opcode_Generator.cpp
+++ SHELLCODE_Opcode_Generator.cpp
@@ -9,13 +9,36 @@
 #include <sstream>
 #include <memoryapi.h>
 #include <errhandlingapi.h>
-#pragma comment(lib, "iphlpapi.lib")
+//#pragma comment(lib, "iphlpapi.lib")
 #include <cmath>
 #include <cstdio>
 #include <ios>
 #include <iosfwd>
 #include <cstdlib>
 #include <regex>
+#include <optional>
+#include <type_traits>
+#include <memory>
+
+
+/////////////////////////////////
+// Define INPUT arguments type
+/////////////////////////////////
+/////////using I_type = char const [5];		//"The C++ Standard forbids containers of const elements because allocator<const T> is ill-formed.";	
+//using I_type = int;						// OK
+using I_type = const char*;				// OK
+//using I_type = char;						// OK
+//using I_type = std::string;				// ?
+
+
+/////////////////////////////////
+// Define OUTPUT return type
+/////////////////////////////////
+using O_type = int;						// OK
+//using O_type = const char*;				// ?
+
+
+
 
 
 // Global definition for creating new arrays of same size
@@ -59,7 +82,6 @@ void read_multi_line_input(std::stringstream& output) {
 		}
 		output << line << '\n';
 	}
-	char aa = '\x55';
 }
 
 void set_xor_key(std::string& xor_key) {
@@ -87,14 +109,37 @@ void hex_string_to_bytes(const std::string& input, unsigned char* output) {
 /// <param name="input">: Multi line input from console stream</param>
 /// <param name="output">: Raw bytes in string format</param>
 void extract_hex_opcode_from_objdump(const std::string& input, std::string& output) {
+	// Prepare the regex pattern for matching hexadecimal bytes
 	std::regex hex_pattern(R"(\b[0-9a-fA-F]{2}\b)");
-	std::sregex_iterator sregex_iterator(input.begin(), input.end(), hex_pattern);
-	std::sregex_iterator end;
+	std::stringstream input_stream(input);
+	std::string line;
 	std::vector<std::string> hexParts;
 
-	while (sregex_iterator != end) {
-		hexParts.push_back(sregex_iterator->str());
-		++sregex_iterator;
+	while (std::getline(input_stream, line)) {
+
+		// Find position of the colon
+		size_t colon_pos = line.find(':');
+
+		// If the colon is found, strip everything before including the colon
+		if (colon_pos != std::string::npos) {
+			line = line.substr(colon_pos + 1);
+		}
+
+		// Find position of the 4 spaces (not the best solution)
+		size_t space4x_pos = line.find("    ");
+		// If 4 spaces are found, strip everything before
+		if (space4x_pos != std::string::npos) {
+			line.resize(space4x_pos);
+		}
+
+		// Apply the regex to the line after the colon
+		std::sregex_iterator sregex_iterator(line.begin(), line.end(), hex_pattern);
+		std::sregex_iterator end;
+
+		while (sregex_iterator != end) {
+			hexParts.push_back(sregex_iterator->str());
+			++sregex_iterator;
+		}
 	}
 
 	// Assemble output byte by byte
@@ -122,6 +167,43 @@ void printAllArguments(Args&&... args) {
 	(printArgument(std::forward<Args>(args)), ...);
 }
 
+//template<typename... Args>
+//int execute_virtual_function_bytes_OLD(const unsigned char* bytes, Args&&... args) {
+//	// Allocate executable memory
+//	LPVOID executableMemory = VirtualAlloc(
+//		NULL,
+//		function_bytes_size,
+//		MEM_COMMIT | MEM_RESERVE,
+//		PAGE_EXECUTE_READWRITE
+//	);
+//
+//	if (executableMemory == NULL) {
+//		std::cerr << "Memory allocation failed: " << GetLastError() << std::endl;
+//		return 1;
+//	}
+//
+//	// Print all arguments before executing the function
+//	std::cout << "Arguments:\n";
+//	printAllArguments(std::forward<Args>(args)...);
+//	std::cout << std::endl;
+//
+//	// Copy function bytes to executable memory
+//	memcpy(executableMemory, bytes, function_bytes_size);
+//
+//	// Cast executable memory to function pointer
+//	auto func_ptr = reinterpret_cast<int(*)(Args...)>(executableMemory);
+//
+//	// Execute function
+//	int result = func_ptr(std::forward<Args>(args)...);
+//
+//	std::cout << "Dynamically executed function returned: " << result << std::endl;
+//
+//	// Free allocated memory
+//	VirtualFree(executableMemory, 0, MEM_RELEASE);
+//
+//	return result;
+//}
+
 /// <summary>
 /// Allocates memory with input bytes, casts (*) to allocated function, calls function with received arguments
 /// </summary>
@@ -129,10 +211,8 @@ void printAllArguments(Args&&... args) {
 /// <param name="bytes">: byte array with function opcode</param>
 /// <param name="...args">: variadic argumets</param>
 /// <returns></returns>
-template<typename ReturnType, typename... Args>
-ReturnType execute_virtual_function_bytes(
-	const unsigned char* bytes,
-	Args&&... args) {
+template<typename O_type, typename... Args>
+std::optional<O_type> execute_virtual_function_bytes(const unsigned char* bytes, Args&&... args) {
 
 	std::cout << "\nExecuting virtual function bytes\n";
 
@@ -146,7 +226,7 @@ ReturnType execute_virtual_function_bytes(
 
 	if (executableMemory == NULL) {
 		std::cerr << "Memory allocation failed: " << GetLastError() << std::endl;
-		return ERROR;
+		return {};
 	}
 
 	// Print all arguments before executing the function
@@ -158,55 +238,108 @@ ReturnType execute_virtual_function_bytes(
 	memcpy(executableMemory, bytes, function_bytes_size);
 
 	// Cast executable memory to function pointer
-	auto func_ptr = reinterpret_cast<ReturnType(*)(Args...)>(executableMemory);
+	auto func_ptr = reinterpret_cast<O_type(*)(Args...)>(executableMemory);
 
 	// Execute function
 	//ReturnType result = func_ptr(std::forward<Args&&>(args)...);
-	ReturnType result = func_ptr(std::forward<Args>(args)...);
+	O_type result = func_ptr(std::forward<Args>(args)...);
 
 	// Display return results
 	print_char('\xcd', 40);
 	std::cout << "Dynamically executed function returned: " << result << std::endl;
 	print_char('\xcd', 40);
 
-
-
-
-	std::cerr << GetLastError() << std::endl;
-
+	//std::cerr << GetLastError() << std::endl;
 
 	// Free allocated memory
 	VirtualFree(executableMemory, 0, MEM_RELEASE);
 
-	return {};
+	return result;
 }
 
+template <typename I_type>
+std::vector<I_type> processInput() {
+	std::vector<I_type> args;
+	std::string input;
+	while (true) {
+
+		std::cout << "Enter argument " << args.size() << ":" << std::endl;
+
+		std::getline(std::cin, input);
+		if (input.empty()) {
+			break;
+		}
+
+		std::istringstream iss(input);
+		std::string str_value;
+
+		if (iss >> str_value) {
+			if constexpr (std::is_same_v<I_type, const char*>) {
+
+				// Create a new copy so it doesn't get lost on the way out
+				char* str_PKc = new char[strlen(str_value.c_str()) + 1];
+				strcpy_s(str_PKc, strlen(str_value.c_str()) + 1, str_value.c_str());
+
+				// Add it to vector
+				args.emplace_back(str_PKc);
+				//args.push_back(str_PKc);
+
+				// cleanup
+			//	delete[] str_PKc;
+			}
+			else if constexpr (std::is_same_v<I_type, std::string>) {
+				I_type value = str_value;
+				args.push_back(value);
+			}
+			else if constexpr (std::is_same_v<I_type, int>) {
+				I_type value = std::stoi(str_value);
+				args.push_back(value);
+			}
+			// OTER TYPES													-> NOT TESTED
+			else {
+				I_type value = {};
+				std::istringstream iss2(input);
+				if (iss2 >> value) {
+					args.push_back(value);
+					//std::cout << value << std::endl;
+				}
+				else {
+					std::cerr << "Invalid input. Please enter a valid type." << std::endl;
+				}
+			}
+		}
+		else {
+			std::cerr << "Invalid input. Please enter a valid type." << std::endl;
+		}
+	}
+	return args;
+}
 
 
 int main() {
 start:
 	std::cout << R"(
 +================================================+
-|/ _\  /\  /\/__\/ /   / /   / __\ /___\/   \/__\|
+|/"_\  /\  /\/__\/"/   /"/   /"__\ /"""\/"""\/__\|
 |\ \  / /_/ /_\ / /   / /   / /   //  // /\ /_\  |
 |_\ \/ __  //__/ /___/ /___/ /___/ \_// /_///__  |
 |\__/\/ /_/\__/\____/\____/\____/\___/___,'\__/  |
-|  /___\_ __   ___ ___   __| | ___               |
-| //  // '_ \ / __/ _ \ / _` |/ _ \              |
+|  /___\_ __   ___ ___   __|"| ___               |
+| //  // "_ \ / __/ _ \ / _` |/ _ \              |
 |/ \_//| |_) | (_| (_) | (_| |  __/              |
-|\___/ | .__/ \___\___/ \__,_|\___|              |
-|   ___|_|                                       |
-|  / _ \___ _ __   ___ _ __ __ _| |_ ___  _ __   |
+|\___/_| .__/ \___\___/ \__,_|\___|              |
+|  /"_"\_|_ _ __   ___ _ __ __ _|"|_ ___  _ __   |
 | / /_\/ _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|  |
 |/ /_\\  __/ | | |  __/ | | (_| | || (_) | |     |
 |\____/\___|_| |_|\___|_|  \__,_|\__\___/|_|     |
 +================================================+
+2025-02-01
 Coded by: Matija Bensa
 )" << '\n';
 	// ASCII generated from: https://www.asciiart.eu/text-to-ascii-art
 
 	std::cout << "File cFunction.cpp should already contain code.\nIf it doesn't first write a code and then come back.\nPress Enter to continue.\n";
-	std::cin.get();
+	//	std::cin.get();
 
 	// Check if g++ exists
 	CHECK(!system("g++ --version > NUL 2>&1"));
@@ -220,7 +353,7 @@ Coded by: Matija Bensa
 
 	// Dump .o file
 	std::cout << "Dumping cFunction.o" << std::endl;
-	system("objdump -d cFunction.o");
+	system("objdump -d cFunction.o -M intel");
 
 	// Get pasted opcode
 	std::stringstream dumped_assembly_code;
@@ -271,27 +404,58 @@ Coded by: Matija Bensa
 	unsigned char* function_bytes_de_ciphered = new unsigned char[function_bytes_size];
 	xor_array(function_bytes_cipher, xor_key.c_str(), function_bytes_size, function_bytes_de_ciphered);
 
-	// And check if bytes are equal
+	// And check memory if bytes after xoring are the same as at beginning
 	if (memcmp(function_bytes, function_bytes_de_ciphered, function_bytes_size)) {
 		std::cout << "XOR-in error!\n";
 		return 1;
 	}
 
-	// Delete function_bytes
+	// Cleanup
 	delete[]function_bytes;
-
-	// Delete function_bytes_cipher
 	delete[]function_bytes_cipher;
+
+	// Input and output types are defined in the "header" of this file
+	std::cout << "\nPredefined INPUT arguments type: <" << typeid(I_type).name() << ">" << std::endl;
+	std::cout << "Predefined OUTPUT result type:   <" << typeid(O_type).name() << ">" << std::endl;
 
 	// Test calling method, set input argument(s)
 	std::cout << "\nCall opcode function with arguments:\n";
-	std::string input;
-	std::getline(std::cin, input);
 
-	// Run decrypted function bytes
-	auto result = execute_virtual_function_bytes<int>(function_bytes_de_ciphered, input.c_str());
+	// Get user input arguments into a vector of Input types
+	std::vector<I_type> args = processInput<I_type>();
 
-	//std::cout << "result: " << result << std::endl;
+
+	// Call the execute_virtual_function_bytes function with the collected arguments and collect the return result
+	std::optional<O_type> result;
+
+	//// Make shared smart pointer so vector doesn't get lost in forwarding
+	//std::shared_ptr<std::vector<I_type>> args_smvp = std::make_shared<std::vector<I_type>>(args);
+
+	//if (args.size() == 1) {
+	//	result = execute_virtual_function_bytes<O_type>(function_bytes_de_ciphered, args_smvp->at(0));
+	//}
+	//else if (args.size() == 2) {
+	//	result = execute_virtual_function_bytes<O_type>(function_bytes_de_ciphered, args_smvp->at(0), args_smvp->at(1));
+	//}
+	//else if (args.size() == 3) {
+	//	result = execute_virtual_function_bytes<O_type>(function_bytes_de_ciphered, args_smvp->at(0), args_smvp->at(1), args_smvp->at(2));
+	//}
+
+	// Call virtual function based on how many input arguments we have
+	if (args.size() == 1) {
+		result = execute_virtual_function_bytes<O_type>(function_bytes_de_ciphered, static_cast<I_type>(args[0]));
+	}
+	else if (args.size() == 2) {
+		result = execute_virtual_function_bytes<O_type>(function_bytes_de_ciphered, static_cast<I_type>(args[0]), static_cast<I_type>(args[1]));
+	}
+	else if (args.size() == 3) {
+		result = execute_virtual_function_bytes<O_type>(function_bytes_de_ciphered, static_cast<I_type>(args[0]), static_cast<I_type>(args[1]), static_cast<I_type>(args[2]));
+	}
+
+	// Again print the return result
+	if (result.has_value()) {
+		std::cout << "result: " << result.value() << std::endl;
+	}
 
 	std::cout << "Press key to exit\n";
 	std::cin.get();
--- cFunction.cpp
+++ cFunction.cpp
@@ -1,79 +1,151 @@
 ﻿
+#include <iostream>
 #include <windows.h>
 #include <iostream>
+#include <cstring>
 
+//##################
+// TEST OUTPUT: INT
+//##################
 
 //
-// TEST INT
+// TEST INPUT: INT
 //
-int test_return_int(){
+int test_01_out_INT_in_2xINT(int num1, int num2){					// OK
+	return num1 + num2;
+}
+
+//
+// TEST INPUT: CHAR
+//
+int test_02_out_INT_in_CHAR(char c){								// OK
+	const char p = 'c';
+	if (p == c)
+		return 1; 
+	return 0;
+}
+
+//
+// TEST INPUT: CONST CHAR*
+//
+int test_03_out_INT_in_PKc(const char* str1) {						// OK
 	return 1;
 }
-// int test_return_int_argument(int in){			// CRASH
-	// return in;
-// }
-// int test_check_char(char c){						// CRASH
-	// const char p = 'c';
-	// if (p != c)
-		// return 1; 
-	// return 0;
-// }
-// int test_check_string_equal(const char* str1, const char* str2, size_t length) {			// CRASH
-	// for (size_t i = 0; i < length; ++i) {
-		// if (str1[i] != str2[i]) {
-			// return 1;
-		// }
-	// }
-	// return 0;
-// }
-// int test_check_password1(const char* str1, size_t length) {			// CRASH
-	// const char str2[] = "var45ssfxkgoof";
-	// for (size_t i = 0; i < length; ++i) {
-		// if (str1[i] != str2[i]) {
-			// return 1;
-		// }
-	// }
-	// return 0;
-// }
-int test_check_password2(const char* str1) {
+int test_04_out_INT_in_PKc(const char* str1) {						// OK
+	if (str1[0] == 'a'){
+		return 0;
+	}
+	return 1;
+}
+int test_05_out_INT_in_PKc(const char* str1) {						// OK
+	int c = 0;
+	for (c = 0; str1[c] != '\0'; ++c){}
+	return c;
+}
+int test_06_out_INT_in_PKc(const char* str1) {						// OK
+	int a = 10;
+	int b = 20;
+	if (a > b){
+		return a;
+	}
+	if (b > a){
+		return b;
+	}
+	return 0;
+}
+int test_07_out_INT_in_VOID(){										// OK
+	return 1;
+}
+int test_08_out_INT_in_INT(int in){									// OK
+	return in;
+}
+int test_09_out_INT_in_PKc(const char* str1) {						// OK
 	const char str2[] = "var45ssfxkgoofrx";
-	size_t c = 0;
+	int c = 0;
 	for (c = 0; str1[c] != '\0'; ++c){}		// count string length
 	if (c != 16){
 		return 1;
 	}
-	for (size_t i = 0; i < 16; ++i) {
+	for (int i = 0; i < 16; ++i) {
 		if (str1[i] != str2[i]) {
 			return 1;
 		}
 	}
 	return 0;
 }
-int test_return_biggest(const char* str1, const char* str2) {			// Always returns -1
-	for (size_t i = 0; str1[i] != '\0' || str2[i] != '\0'; ++i) {
-		if (str1[i] > str2[i]) {
+
+//
+// TEST INPUT: CONST CHAR* & INT		----> Currently not supported different input types
+//
+int test_10_out_INT_in_2xPKc_1xINT(const char* str1, const char* str2, int length) {		// CRASH
+	for (int i = 0; i < length; ++i) {
+		if (str1[i] != str2[i]) {
 			return 1;
-		}
-		if (str2[i] > str1[i]) {
-			return -1;
 		}
 	}
 	return 0;
 }
-int test_return_biggest2(const char str1[32], const char str2[32]) {	// Always returns -1
-	for (size_t i = 0; str1[i] != '\0' || str2[i] != '\0'; ++i) {
-		if (str1[i] > str2[i]) {
+int test_11_out_INT_in_1xPKc_1xINT(const char* str1, int length) {							// CRASH
+	const char str2[] = "var45ssfxkgoof";
+	for (int i = 0; i < length; ++i) {
+		if (str1[i] != str2[i]) {
 			return 1;
-		}
-		if (str2[i] > str1[i]) {
-			return -1;
 		}
 	}
 	return 0;
 }
 
+//##################
+// TEST OUTPUT: INT
+//##################
+
+//
+// TEST INPUT: CONST CHAR*
+//
+const char* test_12_out_PKc_in_PKc(const char* str1){										// CRASH
+	return "OK";
+}
+
+// int test_1_out_INT_in_string(std::string str1) {
+	// std::string str2 = "var45ssfxkgoofrx";
+	// if (str1.size() != 16) {
+		// return 1;
+	// }
+	// for (int i = 0; i < 16; ++i) {
+		// if (str1.c_str()[i] != str2.c_str()[i]) {
+			// return 1;
+		// }
+	// }
+	// return 0;
+// }
+// int test_1_out_INT_in_2xPKc(const char* str1, const char* str2) {			// ?
+	// for (int i = 0; str1[i] != '\0' || str2[i] != '\0'; ++i) {
+		// if (str1[i] > str2[i]) {
+			// return 1;
+		// }
+		// if (str2[i] > str1[i]) {
+			// return -1;
+		// }
+	// }
+	// return 0;
+// }
+// int test_1_out_INT_in_PKc(const char str1[32], const char str2[32]) {		// ?
+	// for (int i = 0; str1[i] != '\0' || str2[i] != '\0'; ++i) {
+		// if (str1[i] > str2[i]) {
+			// return 1;
+		// }
+		// if (str2[i] > str1[i]) {
+			// return -1;
+		// }
+	// }
+	// return 0;
+// }
+
+
+
+
+
 int main(){
-	test_check_password2("aaaaaaaa");
 	return 0;
 }
 
